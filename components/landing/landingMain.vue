ba9dca9f6582e21ad04371e8a726b360d6cdbcec
tyeon99 <xodus170@naver.com>
2024-05-22 15:56:23 +0900

    랜딩 메인페이지 배경 비디오 추가

--- components/landing/landingMain.vue
+++ components/landing/landingMain.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="section landingMainBg">
-    <div class="landingMain">
-      <div class="mainTitle ko">
-        AI 투자금융 테크기업 씽크풀
+  <div class="section">
+    <div class="landingSection">
+      <div class="landingMainBg">
+        <video :src="videoSrc" type="video/mp4" autoplay muted loop>배경비디오</video>
       </div>
-      <div class="mainTitle en">
-        AI Investment Finance Tech Company  THINKPOOL
-      </div>
-      <div class="mainTxt ko">
-        씽크풀은 데이터와 AI시대, 초지능형 과학적 의사결정을 지원하는 지식기업입니다.
-      </div>
-      <div class="mainTxt en">
-        ThinkPool is a knowledge company that supports super-intelligent scientific decision-making in the era of data and AI
+      <div class="landingMain">
+        <div class="mainTitle ko">
+          AI 투자금융 테크기업 씽크풀
+        </div>
+        <div class="mainTitle en">
+          AI Investment Finance Tech Company THINKPOOL
+        </div>
+        <div class="mainTxt ko">
+          씽크풀은 데이터와 AI시대, 초지능형 과학적 의사결정을 지원하는 지식기업입니다.
+        </div>
+        <div class="mainTxt en">
+          ThinkPool is a knowledge company that supports super-intelligent scientific decision-making in the era of data
+          and AI
+        </div>
       </div>
     </div>
   </div>
@@ -19,13 +25,29 @@
 
 <script>
 export default {
-  
+  data() {
+    return{
+      videoSrc: '/tp_web/video/landingMainBg.mp4',
+    }
+  },
 }
 </script>
 
 <style scoped>
+  .landingSection{
+    @apply relative;
+  }
+
+  .landingMainBg{
+    @apply w-full;
+  }
+
+  .landingMainBg video{
+    @apply w-full h-[1080px];
+  }
+
   .landingMain{
-    @apply w-full text-center;
+    @apply w-full text-center absolute top-[45%];
   }
 
   .mainTitle{
