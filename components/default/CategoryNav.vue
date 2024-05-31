2d57b864eff32532de646c3c83cd8deacf6a3c8e
tyeon99 <xodus170@naver.com>
2024-05-31 17:25:26 +0900

    스크롤위치, 카테고리 링크 수정

--- components/default/CategoryNav.vue
+++ components/default/CategoryNav.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="categoryNav">
-    <div class="home">
+    <div class="home" @click="goHome()">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
         <path
           d="M8.306 1.10674C8.21728 1.03756 8.108 1 7.9955 1C7.883 1 7.77372 1.03756 7.685 1.10674L0.5 6.70924L1.1215 7.49524L2 6.81024V12.9997C2.00053 13.2648 2.10606 13.5188 2.29348 13.7063C2.4809 13.8937 2.73495 13.9992 3 13.9997H13C13.2651 13.9992 13.5191 13.8937 13.7065 13.7063C13.8939 13.5188 13.9995 13.2648 14 12.9997V6.81474L14.8785 7.49974L15.5 6.71374L8.306 1.10674ZM9 12.9997H7V8.99974H9V12.9997ZM10 12.9997V8.99974C9.99973 8.7346 9.89429 8.4804 9.70681 8.29292C9.51934 8.10544 9.26513 8 9 7.99974H7C6.73486 8 6.48066 8.10544 6.29319 8.29292C6.10571 8.4804 6.00026 8.7346 6 8.99974V12.9997H3V6.03074L8 2.13574L13 6.03574V12.9997H10Z"
@@ -25,6 +25,7 @@
       v-for="(category, index) in categorys" 
       :key="'category_' + index" 
       :class="{ 'categoryName': true, 'active': index === categoryIndex, 'hidden': index !== categoryIndex }"
+      @click="navigateTo(category.to)"
     >
     <span class="ko">
       {{ category.ko }}
@@ -77,14 +78,14 @@ export default {
   data() {
     return {
       categorys: [
-      { ko: '회사소개', en: 'About Us' },
-      { ko: 'ThinkPool AI', en: 'ThinkPool AI' },
-      { ko: '위치기반서비스', en: 'LBS' },
-      { ko: '리스크관리솔루션', en: 'RMS' },
-      { ko: '기업부설연구소', en: 'R&D Center' },
-      { ko: '홍보센터', en: 'PR' },
-      { ko: 'IR', en: 'IR' },
-      { ko: '채용정보', en: 'Careers' },
+      { ko: '회사소개', en: 'About Us',  to: '/about/corporateOverview'},
+      { ko: 'ThinkPool AI', en: 'ThinkPool AI', to: '/thinkpoolAi/axRassi' },
+      { ko: '위치기반서비스', en: 'LBS', to: '/lbs/loginProtection' },
+      { ko: '리스크관리솔루션', en: 'RMS', to: '/rms/rmsSystem' },
+      { ko: '기업부설연구소', en: 'R&D Center', to: '/rdcenter/rdNational01' },
+      { ko: '홍보센터', en: 'PR', to: '/pr/notice' },
+      { ko: 'IR', en: 'IR', to: '/ir/ir' },
+      { ko: '채용정보', en: 'Careers', to: '/careers/recruit' },
       ],
       menus: [
       { ko: '회사개요', en: 'Corporate Overview' },
@@ -111,16 +112,28 @@ export default {
       ]
     };
   },
+  methods: {
+    navigateTo(path) {
+      this.$router.push(path);
+    },
+    goHome(){
+      this.$router.push('/');
+    }
+  }
 };
 </script>
 
 <style scoped>
+  .home{
+    @apply cursor-pointer;
+  }
+
   .categoryNav{
     @apply mt-[80px] w-full flex justify-start items-center gap-[5px] pl-[250px] pt-[13px];
   }
 
   .categoryName{
-    @apply text-[#b5b5b6] text-[14px] font-normal leading-[21px];
+    @apply text-[#b5b5b6] text-[14px] font-normal leading-[21px] cursor-pointer;
   }
 
   .categoryMenu{
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,26 @@
 <template>
   <div id="wrap">
     <WebHeader />
+    <div ref="Top"></div>
     <Nuxt />
     <WebFooter />
   </div>
 </template>
+
+<script>
+export default{
+  watch: {
+    '$route' () {
+      this.scrollToTop();
+    }
+  },
+  methods: {
+    scrollToTop() {
+      this.$refs.Top.scrollIntoView();
+    }
+  }
+}
+</script>
 
 <style scoped>
 #wrap {
