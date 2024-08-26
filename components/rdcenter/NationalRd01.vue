e3c09f808c7db612f532fb458f3663f5e6e271cb
tyeon99 <xodus170@naver.com>
2024-08-26 09:14:40 +0900

    국가 R&D css 수정

--- components/rdcenter/NationalRd01.vue
+++ components/rdcenter/NationalRd01.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="NationalRd">
+  <div class="rnd">
     <div class="swiper rdSwiper">
       <div class="rdWrap swiper-wrapper">
         <div class="swiper-slide">
@@ -7,9 +7,56 @@
           <div class="titleSpan en">National R&D</div>
           <div class="title ko">서비스 산업융합 고도화 사업</div>
           <div class="title en">Svc Industry Convergence Advancement Project</div>
-          <div class="rdContent">
-            <img src="~/assets/img/RDImg01_ko.png" alt="" class="ko">
-            <img src="~/assets/img/RDImg01_en.png" alt="" class="en">
+          <div class="rndContent">
+            <div>
+              <img src="~/assets/img/rnd.png" alt="rnd" width="446" />
+            </div>
+
+            <div>
+              <div class="tit">
+                <strong>
+                  2023 <br>
+                  산업통상자원부 <br>
+                  지식서비스 개발과제 수행기관 선정
+                </strong>
+                <span>
+                  액티브 시니어 생애설계와<br>
+                  자기계발 지원을 위한 런케이션서비스 개발
+                </span>
+              </div>
+              <!-- // -->
+
+              <div class="info">
+                <div>
+                  <strong>개발기간</strong>
+                  <span>2023년 5월 ~ 2025년 11월</span>
+                </div>
+                <div>
+                  <strong>개발예산</strong>
+                  <span>30억원 투자</span>
+                </div>
+                <div>
+                  <span>주관기관</span>
+                  <span>㈜씽크풀</span>
+                </div>
+                <div>
+                  <strong>참여기관</strong>
+                  <span>(사)금융과 행복 네트워크</span>
+                  <span>(주)프로눔</span>
+                  <span>한국전기통신연구원</span>
+                  <span>상명대 (시니어 휴먼 팩터 연구기관)</span>
+                </div>
+              </div>
+              <!-- // -->
+              <div class="cnc">
+                <a href="#"><img src="~/assets/img/cnc/cnc21.png" alt="씽크풀" width="89"></a>
+                <a href="#"><img src="~/assets/img/cnc/cnc22.png" alt="금융과행복네트워크" width="82"></a>
+                <a href="#"><img src="~/assets/img/cnc/cnc23.png" alt="상명대학교" width="17"></a>
+                <a href="#"><img src="~/assets/img/cnc/cnc24.png" alt="pronum" width="58"></a>
+                <a href="#"><img src="~/assets/img/cnc/cnc25.png" alt="한국전자통신연구원" width="58"></a>
+              </div>
+              <!-- // -->
+            </div>
           </div>
         </div>
         <NationalRd02></NationalRd02>
@@ -26,7 +73,7 @@
 <script>
 export default {
   mounted() {
-    new window.Swiper(".rdSwiper",{
+    new window.Swiper(".rdSwiper", {
       slidesPerView: '1',
       spaceBetween: 100,
       loop: false,
@@ -40,11 +87,62 @@ export default {
 </script>
 
 <style scoped>
-  .NationalRd{
-    @apply flex justify-center items-center px-[100px];
-  }
-
-  .swiper-button-prev, .swiper-button-next{
-    @apply text-[#ccc] w-[109px] h-[109px];
-  }
+.rassiBg {
+  @apply bg-[#EFF2FB];
+}
+.rnd {
+  @apply pt-[90px];
+}
+.rdWrap{
+  @apply p-0;
+}
+.rdWrap .title{
+  @apply mb-0;
+}
+.rndTitle {
+  @apply flex w-full flex-col items-center gap-2;
+}
+.rndTitle em {
+  @apply text-[#9395a1] text-[16px] leading-5;
+}
+.rndTitle strong {
+  @apply text-[#141414] text-[48px] leading-[60px];
+}
+.rndContent {
+  @apply p-[90px_0] flex w-full justify-center items-center gap-[65px];
+}
+.rndContent > div:last-child {
+  @apply flex flex-col w-[700px] h-[899px] justify-center bg-[#fbfbfb] rounded-[20px] p-[0_60px] gap-[5px];
+}
+.rndContent > div .tit {
+  @apply flex flex-col w-full gap-5;
+}
+.rndContent > div .tit strong {
+  @apply text-[28px] leading-[36px] text-[#141414] font-bold;
+}
+.rndContent > div .tit span {
+  @apply text-[16px] leading-[25px] text-black;
+}
+.rndContent div > .info {
+  @apply flex flex-col gap-[22px];
+}
+.rndContent > div .info div {
+  @apply flex flex-col w-full;
+}
+.rndContent > div .info div strong {
+  @apply text-[14px] text-[#141414] leading-[22px];
+}
+.rndContent > div .info div span {
+  @apply text-[#444] text-[14px] leading-[22px];
+}
+.rndContent div .cnc {
+  @apply flex gap-4 w-full pt-[35px];
+}
+.rndContent div .cnc a {
+  @apply w-[105px] h-[32px] border-[1px] border-[#f0f0f0] rounded-[6px] flex justify-center items-center bg-white;
+}
+.swiper-button-prev,
+.swiper-button-next {
+  @apply text-[#ccc] w-[109px] h-[109px];
+}
 </style>
--- pages/rdcenter/rdNational01.vue
+++ pages/rdcenter/rdNational01.vue
@@ -1,68 +1,7 @@
 <template>
   <div class="content">
     <CategoryNav :categoryIndex="categoryIndex" :activeIndex="selectedIndex"></CategoryNav>
-    <div class="rnd">
-      <div class="rndTitle">
-        <strong class="ko">국가R&D</strong>
-        <strong class="en">National R&D</strong>
-      </div>
-      <!-- // -->
-       
-      <div class="rndContent">
-        <div>
-          <img src="~/assets/img/rnd.png" alt="rnd" width="446">
-        </div>
-        <!-- // -->
-         
-        <div>
-          <div class="tit">
-            <strong>
-              2023 <br>
-              산업통상자원부 <br>
-              지식서비스 개발과제 수행기관 선정
-            </strong>
-            <span>
-              액티브 시니어 생애설계와<br>
-              자기계발 지원을 위한 런케이션서비스 개발
-            </span>
-          </div>
-          <!-- // -->
-           
-          <div class="info">
-            <div>
-              <strong>개발기간</strong>
-              <span>2023년  5월 ~ 2025년 11월</span>
-            </div>
-            <div>
-              <strong>개발예산</strong>
-              <span>30억원 투자</span>
-            </div>
-            <div>              
-              <span>주관기관</span>
-              <span>㈜씽크풀</span>
-            </div>
-            <div>
-              <strong>참여기관</strong>
-              <span>(사)금융과 행복 네트워크</span>
-              <span>(주)프로눔</span>
-              <span>한국전기통신연구원</span>
-              <span>상명대 (시니어 휴먼 팩터 연구기관)</span>
-            </div>
-          </div>
-          <!-- // -->
-          <div class="cnc">
-            <a href="#"><img src="~/assets/img/cnc/cnc21.png" alt="씽크풀" width="89"></a>
-            <a href="#"><img src="~/assets/img/cnc/cnc22.png" alt="금융과행복네트워크" width="82"></a>
-            <a href="#"><img src="~/assets/img/cnc/cnc23.png" alt="상명대학교" width="17"></a>
-            <a href="#"><img src="~/assets/img/cnc/cnc24.png" alt="pronum" width="58"></a>
-            <a href="#"><img src="~/assets/img/cnc/cnc25.png" alt="한국전자통신연구원" width="58"></a>
-          </div>
-          <!-- // -->
-        </div>
-        <!-- // -->
-      </div>
-      <!-- // -->
-    </div>
+    <NationalRd01></NationalRd01>
   </div>
 </template>
 
@@ -82,49 +21,4 @@ export default {
   .rassiBg{
     @apply bg-[#EFF2FB];
   }
-.rnd{
-  @apply pt-[90px];
-}
-.rndTitle{
-  @apply flex w-full flex-col items-center gap-2
-}
-.rndTitle em{
-  @apply text-[#9395a1] text-[16px] leading-5
-}
-.rndTitle strong{
-  @apply text-[#141414] text-[48px] leading-[60px]
-}
-.rndContent{
-  @apply p-[90px_0] flex w-full justify-center items-center gap-[65px]
-}
-.rndContent > div:last-child{
-  @apply flex flex-col w-[700px] h-[899px] justify-center bg-[#fbfbfb] rounded-[20px] p-[0_60px] gap-[5px]
-}
-.rndContent > div .tit{
-  @apply flex flex-col w-full gap-5
-}
-.rndContent > div .tit strong{
-  @apply text-[28px] leading-[36px] text-[#141414] font-bold
-}
-.rndContent > div .tit span{
-  @apply text-[16px] leading-[25px] text-black
-}
-.rndContent div  >.info{
-  @apply flex flex-col gap-[22px]
-}
-.rndContent > div .info div{
-  @apply flex flex-col w-full
-}
-.rndContent > div .info div strong{
-  @apply text-[14px] text-[#141414] leading-[22px]
-}
-.rndContent > div .info div span{
-  @apply text-[#444] text-[14px] leading-[22px]
-}
-.rndContent div .cnc{
-  @apply flex gap-4 w-full pt-[35px]
-}
-.rndContent div .cnc a{
-  @apply w-[105px] h-[32px] border-[1px] border-[#f0f0f0] rounded-[6px] flex justify-center items-center bg-white
-}
 </style>
